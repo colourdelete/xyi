205611cd024974954ffe5a101a07989d5538075e
Ken Shibata <kenxshibata@gmail.com>
2021-12-23 17:49:49 -0500

    i18n; quiz; etc

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -6,10 +6,22 @@
 
 import cyrb53 from "/mpm2dp-0a/js/cyrb53.js";
 
-export default function quiz(id, i, ans) {
+function quiz(id, i, ans) {
   const ie = document.getElementById(`quiz-${id}-i${i}`);
   const se = document.getElementById(`quiz-${id}-s${i}`);
   return () => {
     se.innerText = cyrb53(ie.value) === ans ? "✓" : "✗";
   };
 }
+
+function quizzes(es, target) {
+  return () => {
+    let total = "";
+    es.forEach((e) => {
+      total += cyrb53(e.value);
+    });
+    target.innerText = cyrb53(total);
+  };
+}
+
+export { quiz, quizzes };
