bba49cf53eda16cb9eb3fadc254d6e1ec1bfc1c6
Ken Shibata <kenxshibata@gmail.com>
2022-01-04 13:52:29 -0500

    quiz: remember quiz submissions

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -4,6 +4,7 @@
  * @description : for the quiz shortcode
  */
 
+'use strict';
 
 async function digestMessage(message) {
   const encoder = new TextEncoder();
@@ -15,16 +16,38 @@ async function digestMessage(message) {
 function quiz(id, i, ans, page) {
   const ie = document.getElementById(`quiz-${id}-i${i}`);
   const se = document.getElementById(`quiz-${id}-s${i}`);
-  return async () => {
-    localStorage.setItem(`${page}-quiz-${id}-i${i}`, ie.value);
-    se.innerText = (await digestMessage(ie.value)) === ans ? "✓" : "✗";
+  const key = `${page}-quiz-${id}-i${i}`;
+  const value = localStorage.getItem(key);
+  let saved = false;
+  const handler = async () => {
+    se.innerText = ((await digestMessage(ie.value)) === ans ? "✓" : "✗") + (saved ? "remembered" : "");
+    saved = false;
+  };
+  if (value !== null) {
+    console.log(`found saved value in localStorage: ${key}`);
+    ie.value = value;
+    localStorage.setItem(key, ie.value);
+    saved = true;
+    handler();
   }
+  return handler;
 }
 
-function quizzes(es, target) {
+function quizzes(es, target, id, page) {
+  const key = `${page}-quiz-${id}-total`;
+  const value = localStorage.getItem(key);
+  let saved = false;
+  if (value !== null) {
+    console.log(`found saved value in localStorage: ${key}`);
+    target.innerText = value;
+    saved = true;
+  }
   return async () => {
     const total = await Promise.all(es.map(async (e) => await digestMessage(e.value)));
-    target.innerText = await digestMessage(total);
+    const digest = await digestMessage(total);
+    localStorage.setItem(key, digest);
+    target.innerText = digest;
+    saved = false;
   };
 }
 
